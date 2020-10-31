d9d183edc5bc7da6ed931c4a1984ac75558dda4b
Nechir dev <nechirvan.abdullah@outlook.com>
2020-10-31 17:18:59 +0300

    deleting LinkButton Component

--- src/components/ConferenceDetails.js
+++ src/components/ConferenceDetails.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import LinkButton from './LinkButton'
-
-// import '../conferenceDetails.css'
+import { IoIosArrowBack } from 'react-icons/io'
 
 class ConferenceDetails extends React.Component {
     constructor(props) {
@@ -22,32 +20,78 @@ class ConferenceDetails extends React.Component {
                 </div>
                 {/* col 2 */}
                 <div className='right-side'>
-                    <h1 className='conference-name'>{this.props.name}</h1>
-                    <p className='conference-description'>
-                        {this.props.description}
-                    </p>
-                    <h1 className='speakers-label'>Speakers</h1>
-                    <div className='speakers'>
-                        {
-                            this.props.speakers.map(
-                                speaker =>
-                                    <figure key={speaker.id}>
-                                        <img src={speaker.image} alt='speaker1' />
-                                        <figcaption>
-                                            <h3>{speaker.name}</h3>
-                                            <p>{speaker.post + speaker.company}</p>
-                                        </figcaption>
-                                    </figure>
-                            )
-                        }
+                    <div className='detailsSection'>
+                        <h1 className='conference-name'>{this.props.name}</h1>
+                        <p className='conference-description'>
+                            {this.props.description}
+                        </p>
+                        <h1 className='speakers-label'>Speakers</h1>
+                        <div className='speakers'>
+                            {
+                                this.props.speakers.map(
+                                    speaker =>
+                                        <figure key={speaker.id}>
+                                            <img src={speaker.image} alt='speaker1' />
+                                            <figcaption>
+                                                <h3>{speaker.name}</h3>
+                                                <p>{speaker.post + speaker.company}</p>
+                                            </figcaption>
+                                        </figure>
+                                )
+                            }
 
+                        </div>
+                        <div className='call-to-action'>
+                            <button className='register-btn' to='/RegisterForm/'>Register Now</button>
+                            <button className='agenda-btn'>Agenda</button>
+                        </div>
                     </div>
-                    <div className='call-to-action'>
-                        <LinkButton className='register-btn' to='/RegisterForm/'>Register Now</LinkButton>
-                        <LinkButton className='agenda-btn'>Agenda</LinkButton>
+                    <div className='registerSection'>
+                        <div className='top-buttons'>
+                            <button className='back'>
+                                <IoIosArrowBack color='#000' size={30} />Back
+                                </button>
+                            <button className='agenda'>Agenda</button>
+                        </div>
+                        <form className='register-form'>
+                            <ul className='form-elements'>
+                                <li className='form-item'>
+                                    <div className='first_name'>First Name *</div>
+                                    <input type='text' name='first_name' className='form-input-text' />
+                                </li>
+                                <li className='form-item'>
+                                    <div className='last_name'>Last Name *</div>
+                                    <input type='text' name='last_name' className='form-input-text' />
+                                </li>
+                                <li className='form-item'>
+                                    <label className='email'>Email *</label>
+                                    <input type='text' name='email' className='form-input-text' />
+                                </li>
+                                <li className='form-item'>
+                                    <label className='password'>Password</label>
+                                    <input type='text' name='password' className='form-input-text' />
+                                </li>
+                                <li className='form-item'>
+                                    <label className='country'>Country</label>
+                                    <input type='text' name='country' className='form-input-text' />
+                                </li>
+                                <li className='form-item'>
+                                    <label className='job_title'>Job Title</label>
+                                    <input type='text' name='job_title' className='form-input-text' />
+                                </li>
+                            </ul>
+
+                            <div className='conditional_term'>
+                                <input type='checkbox' name='conditional_term' checked />
+                                <label>Agree on conditional terms of use and privacy policies</label>
+                            </div>
+                            <button className='register'>Register</button>
+                        </form>
+                    </div>
+                    <div className='agendaSection'>
+
                     </div>
                 </div>
-
             </div>
         )
     }
